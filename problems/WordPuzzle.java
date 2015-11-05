feec7cc034f84ee44f3f7c272536c2db726f4ee1
Patrik Ackland <patrikackland@gmail.com>
2015-11-04 22:47:12 -0800

    Word puzzle works, passes tests

--- problems/WordPuzzle.java
+++ problems/WordPuzzle.java
@@ -1,18 +1,26 @@
 public class WordPuzzle {
     public static void main(String[] args) {
         char[][] board =
-            new char[][]{{'C','X','Q'},
+            new char[][]{{'F','X','C'},
                          {'F','A','U'},
-                         {'E','P','T'}};
-        System.out.println(hasWord(board, "CAT"));
+                         {'T','P','T'}};
+        assert hasWord(board, "CAT");
+        assert hasWord(board, "F");
+        assert !hasWord(board, "CATA");
+        assert !hasWord(board, "CATAC");
+        assert hasWord(board, "TAPTFFXCU");
+        assert !hasWord(board, "TAPTFFXCUT");
+        System.out.println("✓");
     }
 
     public static boolean hasWord(char[][] b, String x) {
+        if (x == null)
+            throw new IllegalArgumentException();
         for (int i=0; i<b.length; ++i) {
-            for (int j=0; j<b[0].length; ++j) {
+            for (int j=0; j<b.length; ++j) {
                 if (b[i][j] == x.charAt(0)) {
-                    boolean[][] v = new boolean[b.length][b[0].length];
-                    if (play(b, i, j, 1, x, v))
+                    boolean[][] v = new boolean[b.length][b.length];
+                    if (play(b, x, 1, i, j, v))
                         return true;
                 }
             }
@@ -20,20 +28,18 @@ public class WordPuzzle {
         return false;
     }
 
-    public static boolean play(char[][] b, int r, int c, int index, String x, boolean[][] v) {
-        if (index==x.length())
+    private static boolean play(char[][] b, String x, int idx, int r, int c, boolean[][] v) {
+        if (idx == x.length())
             return true;
         v[r][c] = true;
-        for (int i=r-1; i<r+1; ++i) {
-            for (int j=c-1; j<c+1; ++j) {
-                if (i<0 || i>=b.length || j<0 || j>=b[0].length ||
-                        v[i][j] || b[i][j] == x.charAt(index))
+        for (int i=Math.max(0, r-1); i<Math.min(b.length, r+2); ++i) {
+            for (int j=Math.max(0, c-1); j<Math.min(b.length, c+2); ++j) {
+                if ((i==r && j==c) || b[i][j] != x.charAt(idx) || v[i][j])
                     continue;
-                if (play(b,i,j,index+1,x,v))
+                if (play(b, x, idx+1, i, j, v))
                     return true;
             }
         }
-        v[r][c] = false;
         return false;
     }
 }
