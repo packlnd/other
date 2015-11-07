92ee1771886179f4879339acd682781ed2c7c13e
Patrik Ackland <patrikackland@gmail.com>
2015-11-06 17:55:14 -0800

    Completed compress

--- problems/Compress.java
+++ problems/Compress.java
@@ -3,15 +3,26 @@ public class Compress {
         if (input == null)
             throw new IllegalArgumentException();
         StringBuilder sb = new StringBuilder();
-        char currChar = input.charAt(0);
-        for (int i=1; i<input.length(); ++i)
-
+        for (int i=0; i<input.length(); ++i) {
+            char currChar = input.charAt(i);
+            int counter=0;
+            while (i < input.length() && input.charAt(i) == currChar) {
+                ++i;
+                ++counter;
+            }
+            sb.append(currChar).append(counter);
+            --i;
+        }
+        if (sb.length() >= input.length())
+            return input;
         return sb.toString();
     }
 
     public static void main(String[] args) {
         assert compress("AAAA").equals("A4");
         assert compress("AABBBCC").equals("A2B3C2");
+        assert compress("AABBCCDDEE").equals("AABBCCDDEE");
+        assert compress("AABBBCCDDEE").equals("A2B3C2D2E2");
         System.out.println("✓");
     }
 }
