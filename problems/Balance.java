7d1cf94cf64ca7560908a9129b2d651067b55c3f
Patrik Ackland <patrikackland@gmail.com>
2015-11-12 00:57:22 -0800

    Balance brackets

---
+++ problems/Balance.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+class Balance {
+    public static void main(String[] args) {
+        assert brackets("<()>");
+        assert !brackets("<(>)");
+        assert brackets("<([{}])>");
+        assert !brackets("<<<<<<<<<<<<<");
+        assert !brackets(">>>>>>>>>>>>>");
+        assert brackets("<<<<<<<<<<<<<>>>>>>>>>>>>>");
+        System.out.println("✓");
+    }
+
+    public static boolean brackets(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i=0; i<s.length(); ++i) {
+            if ("<{[(".indexOf(s.charAt(i))>=0)
+                stack.push(s.charAt(i));
+            if (">}])".indexOf(s.charAt(i))>=0)
+                if (stack.isEmpty() || !matches(stack.pop(), s.charAt(i)))
+                    return false;
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean matches(char a, char b) {
+        return (a == '[' && b == ']') || (a == '<' && b == '>') ||
+               (a == '(' && b == ')') || (a == '{' && b == '}');
+    }
+}
