f9261c3e6fcd36efaaf70779cc0033dc22d25604
Patrik Ackland <patrikackland@gmail.com>
2016-11-18 13:37:06 +0100

    BST from Array

--- data-structures/BinarySearchTree.java
+++ data-structures/BinarySearchTree.java
@@ -1,205 +1,65 @@
-public class BinarySearchTree<T extends Comparable<T>> {
-    private static class Node<T extends Comparable<T>> {
-        private T data;
-        private Node<T> left;
-        private Node<T> right;
+public class BinarySearchTree {
 
-        private Node(T data) {
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
+  private class Node {
+    private Node left;
+    private Node right;
+    private int data;
+    private Node(int data) {
+      this.data = data;
     }
+  }
 
-    private Node<T> root;
-    private int size;
+  private Node root;
 
-    public BinarySearchTree() {
+  public BinarySearchTree() {
+  }
+
+  public BinarySearchTree(BinarySearchTree other) {
+  }
+
+  public BinarySearchTree(int[] V) {
+    root = createBinarySearchTreeFromArray(V, 0, V.length);
+  }
+
+  private Node createBinarySearchTreeFromArray(int[] V, int lo, int hi) {
+    if (lo == hi)
+      return null;
+    int mid = lo + (hi-lo)/2;
+    Node node = new Node(V[mid]);
+    node.left = createBinarySearchTreeFromArray(V, lo, mid);
+    node.right = createBinarySearchTreeFromArray(V, mid+1, hi);
+  }
+
+  public int depth() {
+  }
+
+  public boolean isBinarySearchTree() {
+  }
+
+  public int heightOfValue(int value) {
+  }
+
+  public void insert(int value) {
+
+  }
+
+  public boolean contains(int value) {
+    if (root==null) {
+      return false;
     }
-
-    public BinarySearchTree(T[] V) {
-        root = insert(V, 0, V.length);
+    Node current = root;
+    while (current != null) {
+      if (current.data == value) {
+        return true;
+      } else if (current.data > value) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
     }
+    return true;
+  }
 
-    private Node<T> insert(T[] V, int low, int high) {
-        if (low >= high)
-            return null;
-        int mid = low+(high-low)/2;
-        Node<T> n = new Node<T>(V[mid]);
-        ++size;
-        n.left = insert(V, low, mid);
-        n.right = insert(V, mid+1, high);
-        return n;
-    }
-
-    public int depth() {
-        return depth(root);
-    }
-
-    private int depth(Node<T> node) {
-        if (node == null)
-            return 0;
-        return Math.max(depth(node.left), depth(node.right))+1;
-    }
-
-    public int leaves() {
-        return leaves(root);
-    }
-
-    private int leaves(Node<T> node) {
-        if (size <= 1) return size;
-        if (node == null) return 0;
-        if (node.left == null && node.right == null) return 1;
-        return leaves(node.left) + leaves(node.right);
-    }
-
-    public void flip() {
-        flip(root);
-    }
-
-    private void flip(Node<T> node) {
-        if (node == null)
-            return;
-        flip(node.left);
-        flip(node.right);
-        Node<T> tmp = node.left;
-        node.left = node.right;
-        node.right = tmp;
-    }
-
-    public void insert(T data) {
-        if (root == null) {
-            ++size;
-            root = new Node<T>(data);
-            return;
-        }
-        insert(data, root);
-    }
-
-    private void insert(T data, Node<T> node) {
-        int compare = data.compareTo(node.data);
-        if (compare == 0)
-            return;
-        if (compare < 0) {
-            if (node.left == null) {
-                node.left = new Node<T>(data);
-                ++size;
-                return;
-            }
-            insert(data, node.left);
-        } else {
-            if (node.right == null) {
-                node.right = new Node<T>(data);
-                ++size;
-                return;
-            }
-            insert(data, node.right);
-        }
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public boolean isBalanced() {
-        return isBalanced(root);
-    }
-
-    private boolean isBalanced(Node<T> node) {
-        return node == null ||
-            (isBalanced(node.left) && isBalanced(node.right) &&
-            Math.abs(depth(node.left)-depth(node.right)) <= 1);
-    }
-
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        toString(sb, root);
-        return sb.toString();
-    }
-
-    private void toString(StringBuilder sb, Node<T> node) {
-        if (node == null)
-            return;
-        sb.append("(");
-        sb.append(node.data);
-        toString(sb, node.left);
-        toString(sb, node.right);
-        sb.append(")");
-    }
-
-    public boolean contains(T value) {
-        return contains(root, value);
-    }
-
-    private boolean contains(Node<T> node, T value) {
-        if (node == null)
-            return false;
-        if (node.data == value)
-            return true;
-        if (contains(node.left, value))
-            return true;
-        if (contains(node.right, value))
-            return true;
-        return false;
-    }
-
-    public T getMax() {
-        if (root == null)
-            return null;
-        Node<T> n;
-        for (n = root; n.right != null; n=n.right);
-        return n.data;
-    }
-
-    public T getMin() {
-        if (root == null)
-            return null;
-        Node<T> n;
-        for (n = root; n.left != null; n=n.left);
-        return n.data;
-    }
-
-    public static void main(String[] args) {
-        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
-        bst.insert(5);
-        bst.insert(2);
-        bst.insert(8);
-        bst.insert(6);
-        /*   5
-         *  / \
-         * 2   8
-         *    /
-         *   6
-         */
-        assert bst.size() == 4;
-        assert bst.toString().equals("(5(2)(8(6)))");
-        assert bst.depth() == 3;
-        assert bst.leaves() == 2;
-        assert bst.contains(5);
-        assert !bst.contains(3);
-        assert bst.getMax() == 8;
-        assert bst.getMin() == 2;
-        bst.flip();
-        assert bst.size() == 4;
-        assert bst.toString().equals("(5(8(6))(2))");
-        assert bst.depth() == 3;
-        assert bst.leaves() == 2;
-        assert bst.contains(5);
-        assert !bst.contains(3);
-        BinarySearchTree<Integer> bst2 =
-            new BinarySearchTree<Integer>(new Integer[]{1,2,3,4,5,6,7});
-        assert bst2.toString().equals("(4(2(1)(3))(6(5)(7)))");
-        assert bst2.depth() == 3;
-        assert bst2.leaves() == 4;
-        assert bst2.size() == 7;
-        assert bst2.getMax() == 7;
-        assert bst2.getMin() == 1;
-        assert bst2.isBalanced();
-        BinarySearchTree<Integer> bst3 = new BinarySearchTree<Integer>();
-        bst3.insert(8);
-        bst3.insert(6);
-        bst3.insert(2);
-        assert !bst3.isBalanced();
-        System.out.println("OK");
-    }
+  public void flip() {
+  }
 }
